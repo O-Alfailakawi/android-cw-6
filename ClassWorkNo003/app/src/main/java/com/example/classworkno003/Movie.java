24452fcac36d1875fbe3821b598f89f6475ab33e
O-Alfailakawi <OmarAlfailakawi02@gmail.com>
2020-07-01 17:48:48 +0300

    Solved CW-6

---
+++ ClassWorkNo003/app/src/main/java/com/example/classworkno003/Movie.java
@@ -0,0 +1,11 @@
+package com.example.classworkno003;
+
+public class Movie {
+
+    private String title;
+   private String mainActor;
+   private double movieRating;
+   private int PGRating;
+  private  String genre;
+
+}
